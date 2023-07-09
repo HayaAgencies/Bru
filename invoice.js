dbf26bf4ff39f8c69944e1eb05baecb712ef4155
rishan987 <riyashasanath@gmail.com>
2023-07-09 17:43:49 +0530

    updates

--- invoice.js
+++ invoice.js
@@ -76,3 +76,20 @@ function FillDataList()
           $(".item_nm").append(Options);               //04
         });
 }
+
+$(document).ready(function () {
+    customer();
+});
+
+function customer()
+{
+        $.getJSON("https://script.google.com/macros/s/AKfycbyg0scZrIPtWIJk-CftfKRcgxnGfunyrVF4wyO4kGJirRNxE9TcFmp2Vaphe3gBQP9o/exec?page=dropdown", 
+        function (data) {                              //01
+          var Options="";                              
+          $.each(data, function(key, value)            //02
+          {
+            Options = Options + '<option>' + value + '</option>';   //03
+          });
+          $(".cust").append(Options);               //04
+        });
+}
